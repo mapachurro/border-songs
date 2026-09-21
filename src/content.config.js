07e596773c8ca9bbf44aad645bbb65e4a07b0b4a
mapachurro <oliver.renwick@gmail.com>
2026-09-21 09:04:15 -0400

    npx astro check; corrected imports, removed deprecated z declarations and usage

--- src/content.config.js
+++ src/content.config.js
@@ -1,5 +1,6 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
+import { z } from 'astro/zod';
 
 const songs = defineCollection({
   loader: glob({
@@ -15,7 +16,7 @@ const songs = defineCollection({
   schema: z.object({
     title: z.string().optional(),
     authority: z.string().optional(),
-    videoSource: z.string().url().optional(),
+    videoSource: z.url().optional(),
   }),
 });
 
